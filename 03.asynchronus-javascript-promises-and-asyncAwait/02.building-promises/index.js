d6ea1e3c2b15ff707c3810ba2053e9224b4f0e6d
MkMonir <khanmonir1335@gmail.com>
2022-01-27 21:58:32 +0600

    Building promises for escape from callback hell

---
+++ 03.asynchronus-javascript-promises-and-asyncAwait/02.building-promises/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const { resolve } = require('path');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 😥');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 😥');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed : ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog img save to file');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
