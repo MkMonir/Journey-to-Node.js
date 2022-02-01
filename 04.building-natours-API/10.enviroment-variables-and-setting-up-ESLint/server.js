4bc999c9d49b7e48264e30637e3fafab10fd7643
MkMonir <khanmonir1335@gmail.com>
2022-02-02 03:09:40 +0600

    Use enviroment variables and setting up ESLint and prettier

---
+++ 04.building-natours-API/10.enviroment-variables-and-setting-up-ESLint/server.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}....`);
+});
