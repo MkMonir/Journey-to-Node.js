e6384f18366ee7b25ad088f0b3687ee89ed922ec
MkMonir <khanmonir1335@gmail.com>
2022-02-17 16:55:11 +0600

    Handling unhandle routes and implementing a global error handling middleware

---
+++ 06.error-handling-with-express/02.implementing-a-global-error-handling-middleware/server.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}....`);
+});
