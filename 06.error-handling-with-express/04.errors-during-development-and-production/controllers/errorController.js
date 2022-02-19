c57b0d6afe4dedc90288977d7defbc14ab5197a5
MkMonir <khanmonir1335@gmail.com>
2022-02-19 18:50:58 +0600

    Error during development and production

---
+++ 06.error-handling-with-express/04.errors-during-development-and-production/controllers/errorController.js
@@ -0,0 +1,35 @@
+const sendErrDev = function (err, res) {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrProd = (err, res) => {
+  // note: OPERATIONAL, trusted error : send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // note: PROGRAMMING or other unknown error: don't leak error details
+  } else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrDev(err, res);
+  }
+};
