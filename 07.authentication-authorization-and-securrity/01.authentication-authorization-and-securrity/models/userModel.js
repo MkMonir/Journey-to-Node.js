3ffae2659c91cb189a8927d9362fd1f72dba62c9
MkMonir <khanmonir1335@gmail.com>
2022-02-21 22:52:13 +0600

    Managing passwords using bcryptjs package

--- 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/models/userModel.js
+++ 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // note: This will only works on .CREATE and .SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // note: ONLY RUN THIS FUNCTION IF PASSWORD WAS ACTUALLY MODIFIED
+  if (!this.isModified('password')) return next();
+
+  // note: HASH THE PASSWORD WITH COST OF 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // note: DELTE PASSWORDCONIRM
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
