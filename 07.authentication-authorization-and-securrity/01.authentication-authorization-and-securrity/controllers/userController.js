e95d4b04ef37c0bb9a62b5eaad73dd87762b9933
MkMonir <khanmonir1335@gmail.com>
2022-02-22 17:43:40 +0600

    Logging in using json web token, bcryptojs and mongoose

--- 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/controllers/userController.js
+++ 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/controllers/userController.js
@@ -1,11 +1,18 @@
+const User = require('../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
 //////////// Users Route handlers
 //// Get Users
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    result: users.length,
+    data: { users },
   });
-};
+});
 
 //// create User
 exports.createUser = (req, res) => {
--- 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/models/userModel.js
+++ 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/models/userModel.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -45,6 +46,10 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
