9673d604273928d8647abd510b2e74c4275df6dc
MkMonir <khanmonir1335@gmail.com>
2022-02-24 01:51:08 +0600

    Sending email with nodemailer using mailtrap

--- 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/controllers/authController.js
+++ 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const sendEmail = require('./../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -17,6 +18,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
     // passwordChangedAt: req.body.passwordChangedAt,
     role: req.body.role,
+    passwordResetToken: req.body.passwordResetToken,
+    passwordResetExpires: req.body.passwordResetExpires,
   });
 
   const token = signToken(newUser._id);
@@ -108,6 +111,28 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) note: Sent it to user's email
+  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password! Submit your PATCH request with your new password and confirmPassword to:${resetURL}.\nif you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 minutes)',
+      text: message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email successfully!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(new AppError('There is an error sending email. Try again later', 500));
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
--- 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/routes/userRoutes.js
+++ 07.authentication-authorization-and-securrity/01.authentication-authorization-and-securrity/routes/userRoutes.js
@@ -8,7 +8,7 @@ router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
 router.route('/forgotPassword').post(authController.forgotPassword);
-router.route('/resetPassword').post(authController.resetPassword);
+router.route('/resetPassword/:token').patch(authController.resetPassword);
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router
