935d5f587bb10934775690b738ae19706cb4293f
MkMonir <khanmonir1335@gmail.com>
2022-03-01 16:27:11 +0600

    Building handler factory function

---
+++ 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/controllers/handlerFactory.js
@@ -0,0 +1,16 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.deleteOne = (model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
