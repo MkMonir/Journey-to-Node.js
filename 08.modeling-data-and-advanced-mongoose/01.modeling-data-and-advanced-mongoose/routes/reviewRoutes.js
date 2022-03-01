c7869d36e54b443bd6b13788489e688dd0db2247
MkMonir <khanmonir1335@gmail.com>
2022-03-01 16:26:27 +0600

    Building handler factory functions for all delete controllers

--- 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/routes/reviewRoutes.js
+++ 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/routes/reviewRoutes.js
@@ -9,4 +9,6 @@ router
   .get(reviewController.getAllReviews)
   .post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
 
+router.route('/:id').delete(reviewController.deleteReview);
+
 module.exports = router;
