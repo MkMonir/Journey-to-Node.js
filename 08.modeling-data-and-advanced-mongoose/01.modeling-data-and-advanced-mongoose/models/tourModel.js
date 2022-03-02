0fde8d213f88f408bdc227d26e4bce1ed71fe3c8
MkMonir <khanmonir1335@gmail.com>
2022-03-02 23:04:38 +0600

    Improving read performance using indexes

--- 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/models/tourModel.js
+++ 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/models/tourModel.js
@@ -118,6 +118,10 @@ const toursSchema = new mongoose.Schema(
   }
 );
 
+// toursSchema.index({ price: 1});
+toursSchema.index({ price: 1, ratingsAverage: -1 });
+toursSchema.index({ slug: 1 });
+
 toursSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
