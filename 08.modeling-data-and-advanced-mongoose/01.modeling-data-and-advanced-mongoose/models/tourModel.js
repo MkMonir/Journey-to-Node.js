eefad088300429df17773e423411e49ee383b76d
MkMonir <khanmonir1335@gmail.com>
2022-03-04 00:41:12 +0600

    Preventing duplicate reviews using mongoose compound index

--- 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/models/tourModel.js
+++ 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/models/tourModel.js
@@ -38,6 +38,7 @@ const toursSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be abobe 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
