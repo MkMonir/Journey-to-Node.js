f7b8ce77cf06eb47c96e0afa5aef74f31e0fd53d
MkMonir <khanmonir1335@gmail.com>
2022-03-03 19:27:30 +0600

    Calculating average rating on new review posted

--- 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/models/reviewModel.js
+++ 08.modeling-data-and-advanced-mongoose/01.modeling-data-and-advanced-mongoose/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -45,8 +46,33 @@ reviewSchema.pre(/^find/, function (next) {
     path: 'user',
     select: 'name photo',
   });
-
   next();
+});
+
+reviewSchema.statics.calcAverageRating = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+reviewSchema.post('save', function () {
+  // note: This points to current review
+  this.constructor.calcAverageRating(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
