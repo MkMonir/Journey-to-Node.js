cffb5da1f6ce8dc324f46d5c378c04f8d0464558
MkMonir <khanmonir1335@gmail.com>
2022-03-05 01:36:49 +0600

    Basic of pug templates

--- 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/app.js
+++ 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/app.js
@@ -70,7 +70,10 @@ app.use((req, res, next) => {
 
 // note: Routes Middleware
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Monir',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
