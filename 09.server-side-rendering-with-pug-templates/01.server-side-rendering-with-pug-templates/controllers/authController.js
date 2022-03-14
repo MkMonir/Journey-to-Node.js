c31feb71fc70f5d1190bffd273206efd570b6f08
MkMonir <khanmonir1335@gmail.com>
2022-03-15 02:26:42 +0600

    Email templates with pug and send welcome email using nodemailer

--- 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/controllers/authController.js
+++ 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -40,10 +40,15 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     // passwordChangedAt: req.body.passwordChangedAt,
-    role: req.body.role,
-    passwordResetToken: req.body.passwordResetToken,
-    passwordResetExpires: req.body.passwordResetExpires,
+    // role: req.body.role,
+    // passwordResetToken: req.body.passwordResetToken,
+    // passwordResetExpires: req.body.passwordResetExpires,
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
+
   createSendToken(newUser, 201, res);
 });
 
@@ -166,11 +171,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password! Submit your PATCH request with your new password and confirmPassword to:${resetURL}.\nif you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 minutes)',
-      text: message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 minutes)',
+    //   text: message,
+    // });
 
     res.status(200).json({
       status: 'success',
