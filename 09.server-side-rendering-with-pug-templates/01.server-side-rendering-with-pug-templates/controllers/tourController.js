bc94abf526ff2e68c0b41db5f681da8bc882650b
MkMonir <khanmonir1335@gmail.com>
2022-03-14 15:12:22 +0600

    Uploading multiple tour images using multer

--- 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/controllers/tourController.js
+++ 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/controllers/tourController.js
@@ -1,7 +1,39 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('./../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images!', 400));
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = (req, res, next) => {
+  console.log(req.files);
+  next();
+};
+
+// note: FOR UPLOADING SINGLE FILE
+// upload.single()
+// note: FOR UPLOADING MULTIPLE SAME NAME FILES
+// upload.array()
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = 5;
