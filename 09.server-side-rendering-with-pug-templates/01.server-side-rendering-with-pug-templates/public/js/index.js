42db3bcdd73e38bbbd0c8d13bca4e5276039fe53
MkMonir <khanmonir1335@gmail.com>
2022-03-14 12:55:57 +0600

    Adding image uploads to form

--- 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/public/js/index.js
+++ 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/public/js/index.js
@@ -33,10 +33,13 @@ if (logOutBtn) logOutBtn.addEventListener('click', logout);
 if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
 
-    updateSettings({ name, email }, 'data');
+    updateSettings(form, 'data');
   });
 
 if (userPasswordForm)
