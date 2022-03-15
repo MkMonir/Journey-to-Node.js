2994c5974287cad327359730d83838ac92d7cbe3
MkMonir <khanmonir1335@gmail.com>
2022-03-16 01:43:15 +0600

    Integrating Strip payment checkout session into the back-end

--- 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/app.js
+++ 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -82,6 +83,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 // Global error handling middleware
 app.all('*', (req, res, next) => {
---
+++ 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/controllers/bookingController.js
@@ -0,0 +1,35 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const Tour = require('./../models/tourModel');
+const factory = require('./handlerFactory');
+
+exports.checkoutSession = catchAsync(async (req, res, next) => {
+  // note: 1) Get the currently booked tour
+  const tour = await Tour.findById(req.params.tourId);
+
+  // note: 2) Create checkout session
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: tour.summery,
+        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        amount: tour.price * 100,
+        currency: 'usd',
+        quantity: 1,
+      },
+    ],
+  });
+
+  // note: 3) Create session as response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
