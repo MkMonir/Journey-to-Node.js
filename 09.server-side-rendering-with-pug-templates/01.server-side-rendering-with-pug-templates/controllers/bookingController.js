f457330b6c6a1516b334c7f2a55bd6e01039360a
MkMonir <khanmonir1335@gmail.com>
2022-03-16 16:13:59 +0600

    Create temporary bookings on checkout session

--- 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/controllers/bookingController.js
+++ 09.server-side-rendering-with-pug-templates/01.server-side-rendering-with-pug-templates/controllers/bookingController.js
@@ -1,7 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
 const Tour = require('./../models/tourModel');
+const Booking = require('./../models/bookingModel');
 
 exports.checkoutSession = catchAsync(async (req, res, next) => {
   // note: 1) Get the currently booked tour
@@ -10,7 +10,9 @@ exports.checkoutSession = catchAsync(async (req, res, next) => {
   // note: 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${
+      req.user.id
+    }&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -31,4 +33,14 @@ exports.checkoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
